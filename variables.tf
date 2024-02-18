cc9399232fc4bdafcfc70937bba1eadc54bb4c1a
root <root@ip-172-31-84-89.ec2.internal>
2024-02-18 08:05:26 +0000

    variables.tf file

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "ami-0e731c8a588258d0d"
+}
+variable "key" {
+  default = "common-key"
+}
+#variable "security_group" {
+#default = "sg-0a96b75ce255bf6a9"
+#}
+variable "type" {
+  default = "t2.medium"
+}
+variable "tags" {
+  default = "Terraform-2024"
+}
